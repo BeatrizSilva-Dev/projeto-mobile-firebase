1e3231481db8cb718a93eb21714bb54120cb3f32
Beatriz Silva <66394398+BeatrizSilva-Dev@users.noreply.github.com>
2023-01-03 21:37:56 -0300

    Alteração e criação do método logout

--- flutter_application_1/lib/home_page.dart
+++ flutter_application_1/lib/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/check_user.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,10 +12,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _firebaseAuth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('HomePage')),
+      body: ListView(
+        children: [
+          Text('HomePage'),
+          TextButton(onPressed: logout, child: Text('Logout'))
+        ],
+      ),
     );
+  }
+
+  logout() async {
+    await _firebaseAuth.signOut().then((User) => Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: ((context) => CheckUser()))));
   }
 }
